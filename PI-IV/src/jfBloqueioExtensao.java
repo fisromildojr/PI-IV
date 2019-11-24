b82c09ae1be1311c146ec31150d5b1798b5356e7
Romildo Jr <r_jrs@DESKTOP-8C82BMI>
2019-11-24 16:21:33 -0300

    Implementação dos botões, já estão abrindo o arquivo (Todos)

--- PI-IV/src/jfBloqueioExtensao.java
+++ PI-IV/src/jfBloqueioExtensao.java
@@ -1,9 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class jfBloqueioExtensao extends JFrame {
 
@@ -29,11 +35,44 @@ public class jfBloqueioExtensao extends JFrame {
 	 * Create the frame.
 	 */
 	public jfBloqueioExtensao() {
+		setTitle("Lista de Extens\u00F5es");
 		setBounds(100, 100, 900, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(0, 0, 872, 543);
+		textArea.setEditable(true);
+		
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		scrollPane.setBounds(0, 0, 872, 543);
+		panel.add(scrollPane);
+		
+		try {
+			BufferedReader arquivo = new BufferedReader(new FileReader("C:/Users/r_jrs/Desktop/squid.conf"));
+			//System.out.println("Arquivo aberto");
+			String str, txt="";
+			while((str = arquivo.readLine()) != null){
+				txt += str + "\n"; //Ele pega a linha coloca uma quebra de linha no final e "concatena" com o que já tem na variavel txt
+			}
+			textArea.setText(txt);
+			scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+			scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+			//scrollPane.add(textArea);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
